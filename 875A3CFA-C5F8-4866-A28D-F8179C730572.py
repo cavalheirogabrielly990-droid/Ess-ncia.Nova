0fe03ae1c471404b9f41435a15557a9034bd3f14
cavalheirogabrielly990-droid <cavalheiro_gabrielly990@escola.pr.gov.br>
2026-09-04 22:30:06 -0300

    Add files via upload

---
+++ 875A3CFA-C5F8-4866-A28D-F8179C730572.py
@@ -0,0 +1,9 @@
+import csv
+# Dados organizados em LISTAS com vírgula (separador padrão do Python)
+dados = [
+["id", "tipo_problema", "local_exemplo", "descricao", "causa_principal", "impacto", "medida_solucao"],
+["1", "Congestionamento", "Av. Paraná - Maringá", "Fluxo parado manhã/tarde", "Alta frota de carros", "Tempo de viagem dobra", "Implantação de faixa reversível"],
+["2", "Transporte Público", "Terminal Centro", "Ônibus lotados e atrasados", "Frota antiga e pouca frequência", "Usuários migram para carros", "Aumentar frequência e renovar frota"],
+["3", "Segurança Viária", "Cruzamento Av. Brasil x Independência", "Acidentes frequentes", "Semáforos desatualizados", "Riscos a pedestres e motoristas", "Sinalização inteligente e faixas de pedestre"],
+["4", "Mobilidade Ativa", "Bairro Zona Sul", "Sem ciclovias e calçadas ruins", "Prioridade a veículos motorizados", "Redução de deslocamentos a pé/de bike", "Construir ciclovias e calçadas acessíveis"],
+["5", "Poluição", "Região Central", "Alta emissão de gases", "Predomínio de veículos a combustão", "Piora da qualidade do ar", "Incentivar carros elétricos e transporte coletivo"] 
